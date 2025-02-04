a46534899f5bd0408be17c010a59930d943d8860
Nosang Subba <nosangdev@gmail.com>
2025-02-04 15:35:39 +0530

    map images in gallery section

--- components/gallery-section.tsx
+++ components/gallery-section.tsx
@@ -3,99 +3,68 @@ import { ThemedView } from "./ThemedView";
 import { StyleSheet } from "react-native";
 import { Image } from "expo-image";
 
+const images = [
+  {
+    name: require("@/assets/placeholder-images/1.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/2.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/3.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/4.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/5.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/6.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/7.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/8.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/1.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/2.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/3.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/4.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/5.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/6.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/7.jpg"),
+  },
+  {
+    name: require("@/assets/placeholder-images/8.jpg"),
+  },
+];
+
 export default function GallerySection() {
   return (
     <ThemedView style={[styles.container, { minHeight: "100%" }]}>
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/1.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/2.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/3.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/4.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/5.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/6.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/7.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/8.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/1.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/2.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/3.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/4.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/5.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/6.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/7.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/8.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/7.jpg")}
-        transition={1000}
-      />
-      <Image
-        style={styles.image}
-        source={require("@/assets/placeholder-images/8.jpg")}
-        transition={1000}
-      />
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          style={styles.image}
+          source={image.name}
+          transition={1000}
+        />
+      ))}
     </ThemedView>
   );
 }
